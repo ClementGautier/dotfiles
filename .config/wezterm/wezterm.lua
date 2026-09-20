69ac227ab8577d96f43671f7edeb1eaeff2e15d3
Clement Gautier <clement.gautier.76@gmail.com>
2026-09-20 15:47:46 +0200

    fix(ui): change font size

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "rose-pine-moon"
 config.font = wezterm.font("Hack Nerd Font")
-config.font_size = 11.0
+config.font_size = 9.0
 config.enable_wayland = false
 config.hide_tab_bar_if_only_one_tab = true
 config.audible_bell = "Disabled"
